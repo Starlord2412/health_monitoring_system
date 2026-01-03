cf90c633f7560de730557f9d3ece3f91e1c28d80
Tejas_057 <tejasjalit@gmail.com>
2026-01-03 14:00:45 +0530

    login pages work

--- src/components/doctor/DoctorLayout.tsx
+++ src/components/doctor/DoctorLayout.tsx
@@ -1,16 +1,22 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import { DoctorNavbar } from "./DoctorNavbar";
 
-export function DoctorLayout() {
+type DoctorLayoutProps = {
+  user?: any;
+};
+
+export const DoctorLayout: React.FC<DoctorLayoutProps> = ({ user }) => {
+  const safeUser = user || null;
+
   return (
     <div className="min-h-screen bg-[#cfeee6]">
-      <DoctorNavbar />
-      {/* inner rounded container like the big white card in the image */}
+      <DoctorNavbar user={safeUser} />
       <main className="mx-auto max-w-6xl px-4 pb-10 pt-8 sm:px-6 lg:px-8">
         <div className="rounded-[28px] bg-white/90 p-6 shadow-[0_18px_60px_rgba(15,23,42,0.10)]">
-          <Outlet />
+          <Outlet context={{ user: safeUser }} />
         </div>
       </main>
     </div>
   );
-}
+};
