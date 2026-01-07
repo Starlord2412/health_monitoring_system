ffc3584fea50c1ac2fd22e7c8c3cc5566fdc91a0
starlord2412 <gayatrifatkar333@gmail.com>
2026-01-07 02:42:46 -0800

    deploy-changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/health_monitoring_system">
+  <BrowserRouter basename="">
     <App />
   </BrowserRouter>
 );
